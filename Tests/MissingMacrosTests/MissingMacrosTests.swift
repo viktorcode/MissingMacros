e1d5e0fb93d1b2aa64603fd64f5e2b3c6782b07e
Viktor Chernikov <viktorcode@icloud.com>
2026-05-24 10:09:40 +0200

    Tests for URL macro

--- Tests/MissingMacrosTests/MissingMacrosTests.swift
+++ Tests/MissingMacrosTests/MissingMacrosTests.swift
@@ -45,4 +45,41 @@ final class MissingMacrosTests: XCTestCase {
         throw XCTSkip("macros are only supported when running tests for the host platform")
         #endif
     }
+
+    func testURLMacroValid() throws {
+        #if canImport(MissingMacrosInternal)
+        assertMacroExpansion(
+            """
+            #url("https://www.apple.com")
+            """,
+            expandedSource: """
+            Foundation.URL(string: "https://www.apple.com")!
+            """,
+            macros: testMacros
+        )
+        #else
+        throw XCTSkip("macros are only supported when running tests for the host platform")
+        #endif
+    }
+
+    func testURLMacroInvalid() throws {
+        #if canImport(MissingMacrosInternal)
+        assertMacroExpansion(
+            """
+            #url("")
+            """,
+            expandedSource: """
+            #url("")
+            """,
+            diagnostics: [
+                .init(message: "URL is malformed: \"\"",
+                      line: 1, column: 1,
+                      severity: .error)
+            ],
+            macros: testMacros
+        )
+        #else
+        throw XCTSkip("macros are only supported when running tests for the host platform")
+        #endif
+    }
 }
